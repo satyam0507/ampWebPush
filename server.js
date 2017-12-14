91a1710b0da4a2a57f49da6f6d429ccf15f7bfe3
satyam0507@gmail.com <satyam0507@gmail.com>
2017-12-14 14:36:06 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+
+// app.use('/static', express.static('static'));
+// app.use('/bower_components', express.static('bower_components'));
+
+app.get('/:name', function(req, res) {
+
+    var options = {
+        root: __dirname + '/dist/',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+
+    var fileName = req.params.name;
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+});
+app.get('/amp/:name', function(req, res) {
+    
+        var options = {
+            root: __dirname + '/amp/',
+            headers: {
+                'x-timestamp': Date.now(),
+                'x-sent': true
+            }
+        };
+    
+        var fileName = req.params.name;
+        res.sendFile(fileName, options, function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Sent:', fileName);
+            }
+        });
+    });
+app.set('port', (process.env.PORT || 5010));
+app.listen(app.get('port'), function() {
+    console.log('Example app listening on port ' + app.get('port'));
+})
